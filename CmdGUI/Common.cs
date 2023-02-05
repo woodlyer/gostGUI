162dd4309b8cfda7864e2c084cfaec3178ac6efd
woodlyer <wood@gmail.com>
2023-02-05 13:04:38 +0800

    init

---
+++ CmdGUI/Common.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+
+
+class Common
+{
+    static public string GetApplicationPath()
+    {
+        string ApplicationPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0].FullyQualifiedName);
+        return ApplicationPath;
+    }
+
+    static public bool SaveToFile(byte[] dataBuf, string fileName)
+    {
+        try
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    bw.Write(dataBuf);
+                    return true;
+                }
+            }
+        }
+        catch (System.Exception)
+        {
+            return false;
+        }
+    }
+
+    static public bool LoadBufFromFile(string fileName, out byte[] dataBuf)
+    {
+        dataBuf = null;
+        try
+        {
+            byte[] data = null;
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    if (fs.Length > 0)
+                    {
+                        data = new byte[fs.Length];
+                        br.Read(data, 0, data.Length);
+                        dataBuf = data;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        catch (System.Exception)
+        {
+            return false;
+        }
+    }
+    static public void TrimString(ref string strLine)
+    {
+        if (string.IsNullOrEmpty(strLine))
+        {
+            return;
+        }
+        try
+        {
+            int commentPos = strLine.IndexOf("//");
+            if (commentPos >= 0)
+            {
+                strLine = strLine.Substring(0, commentPos);
+            }
+            strLine = System.Text.RegularExpressions.Regex.Replace(strLine, "\\s+", " ");
+            strLine.Trim();
+            if (strLine.Length <= 0)
+            {
+                return;
+            }
+            if (strLine[strLine.Length - 1] == ' ' && strLine.Length > 0)
+            {
+                strLine = strLine.Substring(0, strLine.Length - 1);
+            }
+            if (strLine.Length > 0 && strLine[0] == ' ')
+            {
+                strLine = strLine.Substring(1, strLine.Length - 1);
+            }
+        }
+        catch (Exception)
+        {
+            return;
+        }
+    }
+
+    public static bool loadIni(string fullPathName, out Dictionary<string, string> retDic)
+    {
+        retDic = new Dictionary<string, string>();
+
+        string filePath = fullPathName;
+
+        try
+        {
+            Encoding encoding = Encoding.ASCII;// Encoding.UTF8;
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs, encoding))
+                {
+                    string strLine;
+                    while ((strLine = sr.ReadLine()) != null)
+                    {
+                        //TrimString(ref strLine);
+                        if (strLine.Length < 1) { continue; }
+
+                        int pos = strLine.IndexOf('=');
+                        if (pos <= 0)
+                            continue;
+
+                        string name = strLine.Substring(0, pos);
+                        string value = strLine.Substring(pos + 1);
+
+                        TrimString(ref name);
+                        //TrimString(ref value);
+
+                        if (string.IsNullOrEmpty(name))// || string.IsNullOrEmpty(value))
+                            continue;
+
+
+                        name = name.ToLower();
+                        value = value.ToLower();
+
+                        if (!retDic.ContainsKey(name))
+                        {
+                            retDic[name] = value;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        catch (Exception )
+        {
+            //MCommLib.MLog._common.info("{0} 解析异常：{1}", filePath, ex.ToString());
+
+            return false; ;
+        }
+
+    }
+}
+
---
+++ CmdGUI/FormMain.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CmdGUI
+{
+    public partial class FormMain : Form
+    {
+
+        Process p;
+        //StreamWriter input;
+
+        bool startOK = false;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            initFromConfig();
+            Application.ApplicationExit += new EventHandler(this.OnApplicationExit);
+        }
+        void initFromConfig()
+        {
+            //string exeDirPath = System.IO.Path.GetDirectoryName(Common.GetApplicationPath());
+            string exeDirPath = (Common.GetApplicationPath());
+            string path = exeDirPath + "/cmd.conf";
+            Dictionary<string, string> cfgDic;
+            Common.loadIni(path, out cfgDic);
+            if (cfgDic.ContainsKey("program"))
+                textBox1.Text = cfgDic["program"];
+            else
+                textBox1.Text = "gost.exe";
+
+            if (cfgDic.ContainsKey("args"))
+                textBox2.Text = cfgDic["args"];
+            else
+                textBox2.Text = "-L 127.0.0.1:1080 ";
+        }
+
+        void saveConfigToFile()
+        {
+            string cfgStr = "program=" + textBox1.Text + Environment.NewLine;
+            cfgStr += "args=" + textBox2.Text + Environment.NewLine;
+            string cfgFile = Common.GetApplicationPath() + "/cmd.conf";
+            Common.SaveToFile(System.Text.Encoding.ASCII.GetBytes(cfgStr), cfgFile);
+        }
+
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            stop();
+        }
+
+        void start()
+        {
+            string exeFilePath = textBox1.Text;
+            string argsstr = textBox2.Text;
+
+            if (!File.Exists(exeFilePath))
+            {
+                //  MessageBox.(fie.FileName);
+                DialogResult diagorel = MessageBox.Show(this,
+                    "Please inpult right exe path, Program file does not exist!",
+                    "File does not exist!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            saveConfigToFile();
+
+            p = new Process();
+            // 自定义shell
+            p.StartInfo.UseShellExecute = false;
+            // 避免显示原始窗口
+            p.StartInfo.CreateNoWindow = true;
+            // 重定向标准输入（原来是CON）
+            p.StartInfo.WorkingDirectory = Common.GetApplicationPath();
+
+            p.StartInfo.RedirectStandardInput = false;// true;
+            // 重定向标准输出
+            p.StartInfo.RedirectStandardOutput = true;
+            // 数据接收事件（标准输出重定向至此）
+            p.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+
+            p.StartInfo.RedirectStandardError = true;
+            p.ErrorDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+
+            //support process exit event
+            p.EnableRaisingEvents = true;
+            p.Exited += new EventHandler(p_Exit);
+
+            // 界面按钮互锁
+            button2.Enabled = false;
+
+            p.StartInfo.FileName = textBox1.Text;
+            p.StartInfo.Arguments = textBox2.Text;
+
+            startOK = p.Start();
+            if (startOK)
+            {
+                outputAdd("run sucess.");
+                // 重定向输入
+                //input = p.StandardInput;
+                // 开始监控输出（异步读取）
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+                // 界面按钮互锁
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+            else
+            {
+                outputAdd("run failed.");
+            }
+            outputAdd("---------------------------------------------------------");
+        }
+
+        void stop()
+        {
+
+            try
+            {
+                p.Kill();
+                p.Close();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void p_Exit(object sender, System.EventArgs e)
+        {
+            //double runtime  = (p.ExitTime - p.StartTime).TotalMilliseconds;
+            System.Threading.Thread.Sleep(10);
+            update("!!! program exits !!!" + Environment.NewLine);
+            updateButton();
+        }
+        delegate void buttonDelegate();
+        void updateButton()
+        {
+            if (this.InvokeRequired)
+            {
+                try
+                {
+                    Invoke(new buttonDelegate(updateButton), new object[] { });
+                }
+                catch (Exception)
+                {
+                    // nothing to do
+                }
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            update(e.Data + Environment.NewLine);
+        }
+
+
+
+        delegate void updateDelegate(string msg);
+        void update(string msg)
+        {
+            if (this.InvokeRequired)
+            {
+                try
+                {
+                    Invoke(new updateDelegate(update), new object[] { msg });
+                }
+                catch (Exception)
+                {
+                    // nothing to do
+                }
+            }
+            else
+            {
+                textBox3.Text += msg;
+                textBox3.SelectionStart = textBox3.Text.Length - 1;
+                textBox3.ScrollToCaret();
+            }
+        }
+
+        // start
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start();
+        }
+
+        private void outputAdd(string str)
+        {
+            textBox3.AppendText(str);
+            textBox3.AppendText(Environment.NewLine);
+        }
+
+        private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            stop();
+            //p.Kill();
+            //p.Close();
+
+            //try
+            //{
+            //    // 获得指定进程
+            //    Process[] p = Process.GetProcessesByName("brook_windows_amd64.exe");
+            //    // 杀死该进程
+            //    p[0].Kill(); 
+            //    MessageBox.Show("进程关闭成功！");
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("无法关闭此进程！");
+            //}
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 当用户点击窗体右上角X按钮或(Alt+F4)时发生         
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.ShowInTaskbar = false;
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                //this.ShowInTaskbar = false;
+                contextMenuStrip1.Show(Control.MousePosition);
+            }
+
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Visible = true;
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+            }
+        }
+
+
+        //stop 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (startOK)
+            {
+                stop();
+            }
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();       //获得路径
+            textBox1.Text = path;
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void FormMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();       //获得路径
+            textBox1.Text = path;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fie = new OpenFileDialog();
+            //创建对象
+            fie.Title = "select .exe program";
+            //设置文本框标题
+
+            string cdstr = System.Environment.CurrentDirectory;
+            fie.InitialDirectory = cdstr;
+            //对话框的初始目录
+            fie.Filter = "exe|*.exe|all|*.*";
+            //设置文件类型
+            string str = fie.FileName;
+            //获取选择的路径
+            if (fie.ShowDialog() == DialogResult.OK)
+            {
+                //MessageBox.Show(fie.FileName);
+                //用户点击 打开 后要执灯的代码
+                textBox1.Text = fie.FileName;
+            }
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            start();
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            stop();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 结束启动的进程
+            if (startOK)
+            {
+                stop();
+            }
+            notifyIcon1.Dispose();
+            // 整个程序退出
+            Application.Exit();
+        }
+    }
+}
